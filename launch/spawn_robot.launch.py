0158c160440be85c8a12e4c32a42a54625e25f9d
Andreas Serov <serov@uni-bremen.de>
2023-12-12 17:10:44 +0100

    further work on spawning robots

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -1,9 +1,10 @@
 import os
+import yaml
 import sys
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 import numpy as np
 
@@ -11,12 +12,10 @@ import numpy as np
 def get_quaternion_from_euler(roll, pitch, yaw):
     """
     Convert an Euler angle to a quaternion.
-
     Input
       :param roll: The roll (rotation around x-axis) angle in radians.
       :param pitch: The pitch (rotation around y-axis) angle in radians.
       :param yaw: The yaw (rotation around z-axis) angle in radians.
-
     Output
       :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
     """
@@ -28,90 +27,70 @@ def get_quaternion_from_euler(roll, pitch, yaw):
     return [qx, qy, qz, qw]
 
 
-COMMAND_LINE_ARGUMENTS = [
-    'robot_name',
-    'world',
-    'sdf_file',
-    'x',
-    'y',
-    'z',
-    'roll',
-    'pitch',
-    'yaw'
-]
+# Parameter type mapping to infer the correct data type from the cli string
+CLI_PARAM_MAPPING = {
+    'robot_name': str,
+    'world': str,
+    'sdf_file': str,
+    'x': float,
+    'y': float,
+    'z': float,
+    'roll': float,
+    'pitch': float,
+    'yaw': float,
+}
+
+
+def overwrite_yaml_params_from_cli(yaml_params, cli_params):
+    for key, value in cli_params.items():
+        if key in yaml_params and value != '':
+            # Since all parameters from cli in ROS2 are strings, we need to infer the correct data type
+            yaml_params[key] = CLI_PARAM_MAPPING[key](value)
+            # Overwrite the boolean values since they are not correctly parsed, non empty strings are always True
+            if value == 'true' or value == 'True':
+                yaml_params[key] = True
+            elif value == 'false' or value == 'False':
+                yaml_params[key] = False
+    return yaml_params
+
+
+def launch_setup(context):
+    mrg_sim_share_dir = get_package_share_directory('mrg_sim')
+    config_file = os.path.join(mrg_sim_share_dir, 'config', 'spawn_robot.yaml')
+    with open(config_file, 'r') as f:
+        params = yaml.safe_load(f)['spawn_robot']['ros__parameters']
+    params = overwrite_yaml_params_from_cli(params, context.launch_configurations)
+
+    print(f'share dir {mrg_sim_share_dir}')
+    sdf_path = os.path.join(mrg_sim_share_dir, 'models', params['sdf_file'])
+    ign_service_name = '/world/' + params['world'] + '/create'
+    print(f'ign service name {ign_service_name}')
+
+    x, y, z = params['x'], params['y'], params['z']
+    qx, qy, qz, qw = get_quaternion_from_euler(params['roll'], params['pitch'], params['yaw'])
+
+    ign_request_args = 'sdf_filename \"' + sdf_path + '\"' + ', name \"' + params['robot_name'] + \
+        '\"' + f', pose: {{ position: {{ x: {x}, y: {y}, z: {z} }}, orientation: {{ x: {qx}, y: {qy}, z: {qz}, w: {qw} }}}}'
+
+    print(f'request args {ign_request_args}')
+    cmd_string_list = ['ign', 'service', '-s', ign_service_name, '--reqtype', 'ignition.msgs.EntityFactory', '--reptype',
+                       'ignition.msgs.BooleanMsg', '--timeout', '10000', '--req', ign_request_args]
+    print(f'command string list {cmd_string_list}')
+    command_string = ' '.join(cmd_string_list)
+    print(f'command string {command_string}')
+
+    process = ExecuteProcess(
+        cmd=['ign', 'service', '-s', ign_service_name, '--reqtype', 'ignition.msgs.EntityFactory', '--reptype',
+             'ignition.msgs.BooleanMsg', '--timeout', '10000', '--req', ign_request_args],
+        output='screen')
+
+    return [process]
 
 
 def generate_launch_description():
+    launch_description_list = []
+    for param_name, _ in CLI_PARAM_MAPPING.items():
+        launch_description_list.append(DeclareLaunchArgument(param_name, default_value='', description=''))
+    launch_description_list.append(OpaqueFunction(function=launch_setup))
 
-    for arg in sys.argv:
-        print(arg)
-
-    mrg_sim_share_dir = get_package_share_directory('mrg_sim')
-    print('asdjkfhaskljdhalskjdfh')
-    # Declare robot name argument
-    robot_name = DeclareLaunchArgument(
-        'robot_name',
-        default_value='husky',
-        description='Specifies the robot name to be used in ROS topics.')
-    robot_name = LaunchConfiguration('robot_name').perform(None)
-
-    print(robot_name)
-    # Declare world argument
-    world = DeclareLaunchArgument(
-        'world',
-        default_value='challenge',
-        description='Specifies the world to be used in Gazebo.')
-    # Declare sdf file argument
-    sdf = DeclareLaunchArgument(
-        'sdf_file',
-        default_value='COSTAR_HUSKY_SENSOR_CONIG_1/model.sdf',
-        description='Specifies the sdf file to be used in Gazebo.')
-    # Declare robot starting position for each robot argument x y z
-    x = DeclareLaunchArgument(
-        'x',
-        default_value=0.0,
-        description='Specifies the robot starting position in Gazebo.')
-    y = DeclareLaunchArgument(
-        'y',
-        default_value=0.0,
-        description='Specifies the robot starting position in Gazebo.')
-    z = DeclareLaunchArgument(
-        'z',
-        default_value=0.0,
-        description='Specifies the robot starting position in Gazebo.')
-    # Declare robot starting orientation argument roll pitch yaw
-    roll = DeclareLaunchArgument(
-        'roll',
-        default_value=0.0,
-        description='Specifies the robot starting orientation in Gazebo.')
-    pitch = DeclareLaunchArgument(
-        'pitch',
-        default_value=0.0,
-        description='Specifies the robot starting orientation in Gazebo.')
-    yaw = DeclareLaunchArgument(
-        'yaw',
-        default_value=0.0,
-        description='Specifies the robot starting orientation in Gazebo.').value
-
-    sdf_path = os.path.join(mrg_sim_share_dir, 'models', sdf)
-    ign_service_name = '/world/ + ' + world + '/create'
-
-    qx, qy, qz, qw = get_quaternion_from_euler(roll, float(pitch), float(yaw))
-
-    ign_request_args = 'sdf_filename \"' + sdf_path + '\"' + ', name \"' + robot_name + \
-        f', pose: {{ position: {{ x: {x}, y: {y}, z: {z} }}, orientation: {{ x: {qx}, y: {qy}, z: {qz}, w: {qw} }}}}'
-
-    return LaunchDescription([
-        robot_name,
-        world,
-        sdf,
-        x,
-        y,
-        z,
-        roll,
-        pitch,
-        yaw,
-        ExecuteProcess(
-            cmd=['ign', 'service', '-s', ign_service_name, '--reqtype', 'ignition.msgs.EntityFactory', '--reptype',
-                 'ignition.msgs.BooleanMsg', '--timeout', '10000', '--req', ign_request_args],
-            output='screen', arguments=[ign_request_args])])
+    return LaunchDescription(launch_description_list)
