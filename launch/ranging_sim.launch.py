a9408956d3d3d59d18058fd0b40f2f5659bf0d41
Andreas Serov <serov@uni-bremen.de>
2025-04-16 10:51:20 +0200

    updated ranging sim for gazebo harmonic

--- launch/ranging_sim.launch.py
+++ launch/ranging_sim.launch.py
@@ -1,11 +1,12 @@
 import os
-import shutil
 import yaml
 import xml.etree.ElementTree as ET
+import subprocess
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import numpy as np
 
 
@@ -27,24 +28,34 @@ def get_quaternion_from_euler(roll, pitch, yaw):
     return [qx, qy, qz, qw]
 
 
+def get_gazebo_command():
+    ign_result = subprocess.run(['which ign'], shell=True, capture_output=True, text=True)
+    gz_result = subprocess.run(['which gz'], shell=True, capture_output=True, text=True)
+    if ign_result.stdout.strip() != '':
+        print('Detected Ignition Gazebo, trying to use ign command')
+        return ign_result.stdout.strip()
+    elif gz_result.stdout.strip() != '':
+        print('Detected gz Gazebo, trying to use gz command')
+        return gz_result.stdout.strip()
+    
+
 def namespace_sdf_file(sdf_path, namespace):
     tree = ET.parse(sdf_path)
     for plugin in tree.findall('model'):
         plugin.attrib['name'] = namespace
     for plugin in tree.findall('model/plugin'):
-        if plugin.attrib['name'] == 'ignition::gazebo::systems::OdometryPublisher':
+        if plugin.attrib['name'] == 'ignition::gazebo::systems::OdometryPublisher' or \
+           plugin.attrib['name'] == 'gz::sim::systems::OdometryPublisher':
             # Give the odometry publisher a unique topic name
             plugin.find('odom_topic').text = namespace + '/odom_ground_truth'
+            plugin.find('robot_base_frame').text = namespace + '/base_link'
 
     tree.write(file_or_filename=sdf_path, encoding='utf-8', xml_declaration=True)
 
 
-def spawn_model_process(ign_service_name, sdf_path, model_name, x, y, z, qx, qy, qz, qw):
+def ign_spawn_model_process(ign_service_name, sdf_path, model_name, x, y, z, qx, qy, qz, qw):
     ign_request_args = 'sdf_filename: \"' + sdf_path + '\"' + ', name: \"' + model_name + \
         '\"' + f', pose: {{ position: {{ x: {x}, y: {y}, z: {z} }}, orientation: {{ x: {qx}, y: {qy}, z: {qz}, w: {qw} }}}}'
-
-    cmd_string_list = ['ign', 'service', '-s', ign_service_name, '--reqtype', 'ignition.msgs.EntityFactory', '--reptype',
-                       'ignition.msgs.BooleanMsg', '--timeout', '10000', '--req', ign_request_args]
 
     process = ExecuteProcess(
         cmd=['ign', 'service', '-s', ign_service_name, '--reqtype', 'ignition.msgs.EntityFactory', '--reptype',
@@ -62,15 +73,32 @@ def generate_launch_description():
 
     print(f'spawning ranging devices in {params["world"]}')
 
+    gazebo_command = get_gazebo_command()
+
     launch_description_list = []
     # spawn the lander in the rubicon world which also publishes odom message for radio navigation
     ign_service_name = '/world/' + params['world'] + '/create'
     name = params['lander_name']
     sdf_path = os.path.join(mrg_slam_sim_share_dir, 'models', params['lander_sdf_file'])
     x, y, z = params['lander_pose'][0], params['lander_pose'][1], params['lander_pose'][2]
-    qx, qy, qz, qw = get_quaternion_from_euler(params['lander_pose'][3], params['lander_pose'][4], params['lander_pose'][5])
-    lander_process = spawn_model_process(ign_service_name, sdf_path, name, x, y, z, qx, qy, qz, qw)
-    print(f'spawning lander at pose {x}, {y}, {z}')
+    roll, pitch, yaw = params['lander_pose'][3], params['lander_pose'][4], params['lander_pose'][5]
+    qx, qy, qz, qw = get_quaternion_from_euler(roll, pitch, yaw)
+
+    if gazebo_command == 'ign':
+        lander_process = ign_spawn_model_process(ign_service_name, sdf_path, name, x, y, z, qx, qy, qz, qw)
+    elif 'gz' in gazebo_command:
+        lander_process = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 
+                                                       'launch', 'gz_spawn_model.launch.py')),
+            launch_arguments={
+                'world': params['world'],
+                'file': sdf_path,
+                'entity_name': 'lander',
+                'x': str(x), 'y': str(y), 'z': str(z),
+                'roll': str(roll), 'pitch': str(pitch), 'yaw': str(yaw)
+            }.items()
+        )
+    print(f'spawning lander at pose x {x}, y {y}, z {z}, roll {roll}, pitch {pitch}, yaw {yaw}')
     launch_description_list.append(lander_process)
 
     # parse the spawn poses (x, y, z, roll, pitch, yaw) from the yaml file
@@ -89,8 +117,22 @@ def generate_launch_description():
         namespace_sdf_file(sdf_path_tmp, name)
 
         x, y, z = pose[0], pose[1], pose[2]
-        qx, qy, qz, qw = get_quaternion_from_euler(pose[3], pose[4], pose[5])
-        spawn_process = spawn_model_process(ign_service_name, sdf_path_tmp, name, x, y, z, qx, qy, qz, qw)
+        roll, pitch, yaw = pose[3], pose[4], pose[5]
+        qx, qy, qz, qw = get_quaternion_from_euler(roll, pitch, yaw)
+        if 'ign' in gazebo_command:
+            spawn_process = ign_spawn_model_process(ign_service_name, sdf_path_tmp, name, x, y, z, qx, qy, qz, qw)
+        elif 'gz' in gazebo_command:
+            spawn_process = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 
+                                                           'launch', 'gz_spawn_model.launch.py')),
+                launch_arguments={
+                    'world': params['world'],
+                    'file': sdf_path_tmp,
+                    'entity_name': name,
+                    'x': str(x), 'y': str(y), 'z': str(z),
+                    'roll': str(roll), 'pitch': str(pitch), 'yaw': str(yaw)
+                }.items()
+            )
         launch_description_list.append(spawn_process)
 
     # create a ros_gz_bridge which creates a config file for the radio boxes on the fly
